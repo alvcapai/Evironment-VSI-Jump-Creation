c97281455d2a2667ee68ee47bea19b60689c6836
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-12-10 14:49:14 -0300

    x

--- main.tf
+++ main.tf
@@ -152,8 +152,8 @@ resource "ibm_is_vpc_routing_table_route" "tgw_route" {
   name          = "${var.name_prefix}-to-tgw"
   destination   = var.transit_gateway_destination_cidr
   action        = "delegate"
-  # Use the transit gateway connection ID for delegation.
-  next_hop      = ibm_tg_connection.vpc.id
+  # Use the transit gateway connection_id (bare UUID) as next hop for delegation.
+  next_hop      = ibm_tg_connection.vpc.connection_id
 
   depends_on = [ibm_tg_connection.vpc]
 }
