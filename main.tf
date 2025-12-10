4a643854e42cedef311e2c7423d2b182e1dd9c95
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-12-10 16:16:54 -0300

    x

--- main.tf
+++ main.tf
@@ -151,8 +151,8 @@ resource "ibm_is_vpc_routing_table_route" "tgw_route" {
   zone          = local.zone
   name          = "${var.name_prefix}-to-tgw"
   destination   = var.transit_gateway_destination_cidr
-  # Transit Gateway route: delegate via the TG connection.
-  action   = "delegate"
+  # Transit Gateway route: deliver to the TG connection (API expects deliver+connection_id).
+  action   = "deliver"
   next_hop = ibm_tg_connection.vpc.connection_id
 
   depends_on = [ibm_tg_connection.vpc]
