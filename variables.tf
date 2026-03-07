039022d9fe6817ed07807a44e9cbe21ebab83672
Alvaro Capai <alvaro.a.capai@gmail.com>
2026-03-07 14:29:52 -0300

    fixz

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "vpc_cidr" {
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet."
   type        = string
-  default     = "10.240.0.0/24"
+  default     = "10.240.2.0/24"
 }
 
 variable "private_subnet_cidr" {
