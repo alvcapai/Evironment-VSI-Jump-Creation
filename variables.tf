aa644449ee7acf3fc94d8835d6f4591cde8f3f82
Alvaro Capai <alvaro.a.capai@gmail.com>
2026-03-07 14:24:27 -0300

    fix networks

--- variables.tf
+++ variables.tf
@@ -34,13 +34,13 @@ variable "vpc_cidr" {
 variable "public_subnet_cidr" {
   description = "CIDR block for the public subnet."
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.240.0.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet."
   type        = string
-  default     = "10.0.101.0/24"
+  default     = "10.240.1.0/24"
 }
 
 variable "allowed_admin_cidr" {
